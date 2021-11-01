6b935b8f5da49dbcead108a824855f5d2b722437
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-01 16:42:42 -0400

    Methods to load files and create dataset

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ import re
 from glob import glob
 from os import path
 
+import tensorflow as tf
+
 
 class SegmentationDataset:
 
@@ -24,7 +26,7 @@ class SegmentationDataset:
         self.resize_interpolation = resize_interpolation
 
         self.image_segmentation_filepath_pairs = self._group_images_to_segmentations()
-        print("yay")
+        self.dataset = self._create_dataset()
 
     def _group_images_to_segmentations(self) -> list[tuple[str, str]]:
         """
@@ -76,3 +78,37 @@ class SegmentationDataset:
                 indexed_file_ids.append((file_id[0], index))
 
         return indexed_file_ids
+
+    def _create_dataset(self):
+        """
+        Create a tf.data dataset with the list of files
+        """
+        file_pairs_tensor = tf.convert_to_tensor(self.image_segmentation_filepath_pairs)
+        dataset = tf.data.Dataset.from_tensor_slices(file_pairs_tensor)
+        dataset = dataset.map(self._load_images)
+        dataset = dataset.batch(self.batch_size)  # Auto tune ?
+        return dataset
+
+    def _load_images(self, file_pairs: tuple[str, str]):
+        """
+
+        :param file_pairs:
+        :return:
+        """
+        image_file = tf.io.read_file(file_pairs[0])
+        segmentation_file = tf.io.read_file(file_pairs[1])
+
+        image = tf.image.decode_jpeg(image_file, channels=3)
+        segmentation = tf.image.decode_jpeg(segmentation_file, channels=3)
+
+        image = tf.cast(image, tf.float32) * (1.0 / 255.0)
+        segmentation = tf.cast(segmentation, tf.float32) * (1.0 / 255.0)
+
+        image = tf.image.resize(image, size=self.image_size, method=self.resize_interpolation, antialias=False)
+        segmentation = tf.image.resize(segmentation, size=self.image_size, method=self.resize_interpolation,
+                                       antialias=False)
+
+        return image, segmentation
+
+    def get_dataset(self):
+        return self.dataset
