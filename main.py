e378cd58ff11d7f862d0b13d6f5f5723e2fc9681
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-01 16:43:18 -0400

    Benchmark new implementation vs old one

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import tensorflow as tf
-
+import time
 from data import SegmentationDataset
+import matplotlib.pyplot as plt
+
+
+def benchmark(dataset, num_epochs=1):
+    start_time = time.perf_counter()
+    for epoch_num in range(num_epochs):
+        i = 0
+        for sample in dataset:
+            i += 1
+            if i % 20 == 0:
+                print("sample #" + str(i))
+            # Performing a training step
+            time.sleep(0.01)
+            if i == 300:
+                break
+    print("Execution time:", time.perf_counter() - start_time)
+
 
 if __name__ == '__main__':
     # msg = tf.constant('TensorFlow 2.0 Hello World')
@@ -9,6 +26,12 @@ if __name__ == '__main__':
     images_directory = "C:/Users/0xD34DC0DE/Pictures/ASMLGen/output_test/images/person/"
     segmentations_directory = "C:/Users/0xD34DC0DE/Pictures/ASMLGen/output_test/annotations/person/"
 
-    dataset = SegmentationDataset(images_directory, segmentations_directory, file_id_regex="(\\d*)\\w*\\.")
+    dataset = SegmentationDataset(images_directory, segmentations_directory,
+                                  file_id_regex="(\\d*)\\w*\\.").get_dataset()
+    print("benchmarking")
+    benchmark(dataset)
 
-#8:20 - 11:48
+    i, s = next(iter(dataset))
+    plt.imshow(i[0])
+    plt.show()
+# 8:20 - 11:48
