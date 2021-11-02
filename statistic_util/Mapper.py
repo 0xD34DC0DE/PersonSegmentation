6dd21c5710dfa5657e9994550e691c2f88b8c765
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-02 01:29:11 -0400

    temporary commenting code since dynamic attribute cannot be tested at the moment

--- statistic_util/Mapper.py
+++ statistic_util/Mapper.py
@@ -1,25 +1,30 @@
 from statistic_util import Producer
+from os import getpid
 
 
 class Mapper:
     def __init__(self):
         pass
 
-    def map(self, mapper_id, mapping_depth, producer: Producer):
-        transfer_attr = self._get_transfer(mapper_id)
-        item = producer.next_item() if mapping_depth == 0 else getattr(producer, transfer_attr)
+    def map(self, item, mapping_depth, producer: Producer) -> None:
+        # transfer_attr = self._get_transfer(producer)
+        # if mapping_depth != 0:
+        #     getattr(producer, transfer_attr)
         item = self._process(item)
 
         next_mapper = producer.get_next_mapper(mapping_depth)
         if next_mapper is None:
             producer.collect(item)
         else:
-            setattr(producer, transfer_attr, item)
-            next_mapper(mapper_id, mapping_depth + 1, producer)
+            #setattr(producer, transfer_attr, item)
+            next_mapper.map(item, mapping_depth + 1, producer)
 
     @staticmethod
-    def _get_transfer(mapper_id) -> str:
-        return f'transfer{mapper_id}'
+    def _get_transfer(producer: Producer) -> str:
+        transfer_str = f'transfer{getpid()}'
+        if not hasattr(producer, transfer_str):
+            setattr(producer, transfer_str, None)
+        return transfer_str
 
     @staticmethod
     def _process(item):
