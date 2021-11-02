d46d2b2a6d34b5d42aa43711b2478dda2665d839
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-02 01:29:49 -0400

    reimplementation of Producer to use threading instead of multiprocessing because of broken implementation

--- statistic_util/Producer.py
+++ statistic_util/Producer.py
@@ -1,25 +1,26 @@
 from __future__ import annotations
 
-from statistic_util import Mapper
-import multiprocessing
+import concurrent
+from concurrent import futures
 
+from statistic_util import Mapper
+import threading
+from multiprocessing import cpu_count
+import traceback
+import logging
 
 class Producer:
 
     def __init__(self, producer_impl: Producer, items: list):
         self._mappers: list[Mapper] = []
         self._results = []
-        self._items = []
+        self._items = items
         self._producer_impl: Producer = producer_impl
-        self._lock: multiprocessing.Lock = multiprocessing.Lock()
+        self._iter_lock: threading.Lock = threading.Lock()
+        self._result_lock: threading.Lock = threading.Lock()
         # Most modern system use virtual cores (aka hyperthreading) but compute heavy tasks wont benefit from this,
         # so divide the available number of cores by two to get the physical core count
-        self._thread_count = multiprocessing.cpu_count() // 2
-        self._add_transfer_attributes(self._thread_count)
-
-    def _add_transfer_attributes(self, attribute_count: int):
-        for i in range(attribute_count):
-            setattr(self, f'transfer{i}', any)
+        self._thread_count = cpu_count() // 2
 
     def get_next_mapper(self, mapping_depth) -> Mapper:
         if mapping_depth == len(self._mappers) - 1:
@@ -27,20 +28,39 @@ class Producer:
         else:
             return self._mappers[mapping_depth + 1]
 
-    def collect(self, result):
-        self._lock.acquire()
-        self._results.append(result)
-        self._lock.release()
+    @staticmethod
+    def _call_first_mapper(self, item):
+        return self._mappers[0].map(item, 0, self)
 
-    def next_item(self):
-        self._lock.acquire()
+    def collect(self, result):
+        with self._result_lock:
+            self._results.append(result)
+
+    def _next_item(self):
         for item in self._items:
-            self._lock.release()
-            yield self._producer_impl._produce_item(item)
+            yield item
 
     def map(self, mapper: Mapper) -> Producer:
         self._mappers.append(mapper)
         return self
+
+    def get_result(self):
+        if not self._results:
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                future_list = []
+                for item in self._next_item():
+                    future_list.append(
+                        executor.submit(self._mappers[0].map, mapping_depth=0, producer=self, item=item)
+                    )
+
+                for future in future_list:
+                    try:
+                        future.result()
+                    except Exception as e:
+                        logging.error(traceback.format_exc())
+
+                # executor.map(self._call_first_mapper, self)
+        return self._results
 
     @staticmethod
     def _produce_item(self, item):
