e9417d24794326ece54eccb01f797987181887f1
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-01 21:47:00 -0400

    moved code to superclass

--- statistic_util/DirectoryIterator.py
+++ statistic_util/DirectoryIterator.py
@@ -1,29 +1,25 @@
-
-
 from glob import glob
 import numpy as np
 import tensorflow as tf
-import multiprocessing
 
-from statistic_util.Mapper import Mapper
+from statistic_util.Producer import Producer
 
 
-class DirectoryIterator(object):
+class DirectoryIterator(Producer):
 
     def __init__(self, directory: str):
-        self.directory = self.sanitize_path(directory)
+        self._directory = self._sanitize_path(directory)
 
-        self.directory_files = glob(directory + "*")
-        self.file_iterator = iter(self.directory_files)
+        self._directory_files = glob(directory + "*")
 
-    def sanitize_path(self, directory: str) -> str:
+        super().__init__(self, self._directory_files)
+
+    @staticmethod
+    def _sanitize_path(directory: str) -> str:
         if not directory[-1] == '\\' or not directory[-1] == '/':
             directory += '/'
         return directory
 
-    def _load_image(self, filepath: str) -> np.ndarray:
+    @staticmethod
+    def _produce_item(filepath: str) -> np.ndarray:
         return tf.image.decode_jpeg(tf.io.read_file(filepath)).numpy
-
-    def __iter__(self) -> np.ndarray:
-        for file_path in self.directory_files[:3000]:
-            yield self._load_image(file_path)
