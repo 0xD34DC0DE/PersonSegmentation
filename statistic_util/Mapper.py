972f0881f2e55db32123f205453910e9228086de
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-01 21:46:14 -0400

    implemented Mapper base class

--- statistic_util/Mapper.py
+++ statistic_util/Mapper.py
@@ -1,7 +1,26 @@
+from statistic_util import Producer
+
 
 class Mapper:
     def __init__(self):
         pass
 
-    def map(self, mapper_id, transfer_object):
-        pass
+    def map(self, mapper_id, mapping_depth, producer: Producer):
+        transfer_attr = self._get_transfer(mapper_id)
+        item = producer.next_item() if mapping_depth == 0 else getattr(producer, transfer_attr)
+        item = self._process(item)
+
+        next_mapper = producer.get_next_mapper(mapping_depth)
+        if next_mapper is None:
+            producer.collect(item)
+        else:
+            setattr(producer, transfer_attr, item)
+            next_mapper(mapper_id, mapping_depth + 1, producer)
+
+    @staticmethod
+    def _get_transfer(mapper_id) -> str:
+        return f'transfer{mapper_id}'
+
+    @staticmethod
+    def _process(item):
+        raise NotImplementedError
